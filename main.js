a1eb4b45fba3538abb9d91c40bbd23e06079a535
gracechao <2638134535@qq.com>
2021-05-24 22:47:37 +0800

    首页开发完成

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ import App from './App'
 Vue.config.productionTip = false
 //引入全局组件
 import divider from './components/common/divider.vue';
-
 Vue.component('divider',divider)
+import noThing from './components/common/no-thing.vue';
+Vue.component('no-thing',noThing)
 App.mpType = 'app'
 
 const app = new Vue({
