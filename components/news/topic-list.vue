c48f64e04539cba75298163467b800eff8d43834
gracechao <2638134535@qq.com>
2021-06-02 13:28:24 +0800

    15.2

--- components/news/topic-list.vue
+++ components/news/topic-list.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="flex align-center  p-2" >
+	<view class="flex align-center  p-2" @click="open">
 		<image :src="item.cover" style="width: 150rpx; height: 150rpx;" class="rounded mr-2"></image>
 		<view class="flex flex-column flex-1 " >
 			<text class="font-md text-dark">{{item.title}}</text>
@@ -17,6 +17,14 @@
 		props:{
 			item:Object,
 			index:Number
+		},
+		methods:{
+			open(){
+				uni.navigateTo({
+					url: '../../pages/topic-detail/topic-detail?detail='+JSON.stringify(this.item),
+					
+				});
+			}
 		}
 	}
 </script>
---
+++ pages/topic-detail/topic-detail.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<view class="">
+			<image src="/static/demo/topicpic/1.jpeg" mode="aspectFill" style="height: 300rpx;" class="w-100"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		onLoad(e) {
+			console.log(e.detail)
+			let res =JSON.parse(e.detail)
+			console.log(res)
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
