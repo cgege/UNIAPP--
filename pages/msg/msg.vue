5a9456c612979905385f4e5fe5e53ca84b9f85d5
gracechao <2638134535@qq.com>
2021-06-24 14:05:40 +0800

    17

--- pages/msg/msg.vue
+++ pages/msg/msg.vue
@@ -9,6 +9,17 @@
 		<template v-else>
 			<no-thing></no-thing>
 		</template>
+		
+		<!-- 弹出层-->
+		<uni-popup ref="popup" type="top" >
+			<view class="bg-white flex justify-center align-center font-md border-bottom py-2 " hover-class="bg-light" @click="popupEvent('friend')">
+				<text class="iconfont icon-sousuo mr-2"></text>添加好友
+			</view>
+			<view class="bg-white flex justify-center align-center font-md border-bottom py-2" hover-class="bg-light" @click="popupEvent('cleak')">
+				<text class="iconfont icon-shanchu mr-2"></text>清除列表
+			</view>
+			
+		</uni-popup>
 	</view>
 </template>
 
@@ -54,15 +65,28 @@
 	
 	import msgList from '@/components/msg/msg-list.vue';
     import noThing from '@/components/common/no-thing.vue';
+	import uniPopup from '@/components/uni-ui/uni-popup/uni-popup.vue';
 	export default {
 		components:{ 
 			msgList,
-			noThing
+			noThing,
+			uniPopup
 		},
 		data() {
 			return {
 				list:[]
 			};
+		},
+		// 监听原生导航栏按钮
+		onNavigationBarButtonTap(e) {
+			switch(e.index){
+				case 0:
+				this.$refs.popup.close()
+					break;
+				case 1:
+				this.$refs.popup.open()
+					break;	
+			}
 		},
 		onPullDownRefresh() {
 		  console.log(111)
@@ -79,6 +103,16 @@
 					// 停止下拉刷新
 					uni.stopPullDownRefresh()
 				},2000)
+			},
+			popupEvent(e){
+				switch (e){
+					case 'friend':
+						break;
+					case 'cleak':
+					console.log(123)
+						break;
+				}
+				this.$refs.popup.close()
 			}
 		}
 		
